a42984b3a2e7f710c45e68f325268bc434ebbe21
setouchi-h <setouchi.kazuki@gmail.com>
2024-04-27 19:22:55 +0900

    add: html and css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
@@ -6,8 +6,8 @@ print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=5050)

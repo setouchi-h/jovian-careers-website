06474a4e0e7ee80dfa769b1fa66bc88336e38b66
setouchi-h <setouchi.kazuki@gmail.com>
2024-04-27 20:02:30 +0900

    update: html and css

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
-print(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Tokyo, Japan',
+        'salary': '¥500,000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Osaka, Japan',
+        'salary': '¥400,000',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'New York, USA',
+        'salary': '$120,000',
+    },
+]
 
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':

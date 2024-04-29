8e3dc06e0e47ff7f852e4f9f05f2c219f3d3671a
setouchi-h <setouchi.kazuki@gmail.com>
2024-04-30 01:06:20 +0900

    add: application handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, jsonify, render_template, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -18,6 +18,16 @@ def show_job(id):
         return "Job not found", 404
 
     return render_template('jobpage.html', job=job)
+
+
+@app.route('/job/<id>/apply', methods=['POST'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+
+    add_application_to_db(id, data)
+
+    return render_template('application_submitted.html', application=data, job=job)
 
 
 @app.route('/api/jobs')
--- database.py
+++ database.py
@@ -25,9 +25,36 @@ def load_job_from_db(id):
         result = conn.execute(
             text('SELECT * FROM jobs where id = :id'), {'id': id})
         rows = result.all()
-        print(result)
         if len(rows) == 0:
             return None
         else:
-            print(rows[0]._mapping)
             return rows[0]._mapping
+
+
+# def add_application_to_db(job_id, data):
+#     with engine.connect() as conn:
+#         query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+#         conn.execute(query,
+#                      {'job_id': job_id,
+#                       'full_name': data['full_name'],
+#                       'email': data['email'],
+#                       'linkedin_url': data['linkedin_url'],
+#                       'education': data['education'],
+#                       'work_experience': data['work_experience'],
+#                       'resume_url': data['resume_url']})
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+        conn.execute(query, {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url']
+        })
+        conn.commit()
